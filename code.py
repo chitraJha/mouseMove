04b51a124b074d106b01cb0d3369049abcac64cb
Chitra Jha <59146250+chitraJha@users.noreply.github.com>
2020-10-17 00:59:38 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+for i in range(0,100):
+    pyautogui.moveTo(1000,1000, duration = 1)
+    time.sleep(10)
+    pytautogui.moveTo(100,100, duration = 1)
+    time.sleep(10)
+    
+    print(i)
